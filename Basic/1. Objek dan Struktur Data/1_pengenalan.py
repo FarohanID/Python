d230e7997ba1b24341e5b68624019e4eb545ffbb
Ahmad Farhan <ahmad.farohan@gmail.com>
2025-08-10 20:54:49 +0700

    Pengenalan Objek dan Struktur Data
    Membuat file 1_pengenalan.py yang berisi materi Pengenalan Objek dan Struktur Data dari Udemy dalam bentuk komentar

---
+++ Basic/1. Objek dan Struktur Data/1_pengenalan.py
@@ -0,0 +1,40 @@
+# Tipe data pada Python
+
+# Numeric Type: int, float, complex
+# Boolean Type: bool
+# Text Sequence Type: string
+# Sequence Type: list, tuple
+# Set Type: set, frozenset
+# Mapping Type: dict
+
+# Contoh Numeric Type
+# Integers (int) = -20, 0, 24, 113144142
+# Floating Point (float) = -.2.5, 0.0001, 123.12, 20.0
+# Complex Number (complex) = 2 + 3j, 3 - 5j
+
+# Contoh Boolean Type
+# bool = True
+# bool = False
+
+# Contoh Text Sequence Type
+# string "hello"
+# string 'Indonesia Merdeka'
+
+# Contoh Sequence Type
+# list = [1, 2, 3, 4, 5, 334, 34]
+# list = ['a', 2, 4.0, -2]
+# tuple = (1, 4, 312, 22)
+# tuple = ('mangga', 2.1, -1)
+
+# Contoh Set Type
+# set {2, 3, 4}
+# set {"a", "b"}
+# frozenset {1, 2, 3, 4}
+# frozenset {"a", "b"}
+
+# Contoh Mapping Type
+# Dictionary (dict) {1: "Soekarno", 2:"Soeharto"}
+
+# Build-In Function adalah fungsi yang sudah ada di dalam Python itu sendiri
+# Contohnya: print(), type(), len() dan sebagainya
+# Untuk lebih lanjut bisa kunjungi https://docs.python.org/3/library/functions.html
